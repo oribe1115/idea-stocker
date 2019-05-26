21777d42a60a00ae56edc20c1e421a78f5cef5e6
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-26 19:13:58 +0900

    gormでtableを作る関数を書いてみた

--- main.go
+++ main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo"
 )
 
+type Ideas struct {
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	Status    string
+	Idea      string
+}
+
+var (
+	db *gorm.DB
+)
+
 func main() {
+	databaseURL := os.Getenv("DATABASE_URL")
+	_db, err := gorm.Open("postgres", databaseURL)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db = _db
+	defer db.Close()
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
 	})
+
+	e.GET("/create", makeTable)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -20,4 +44,10 @@ func main() {
 	}
 
 	e.Start(":" + port)
+}
+
+func makeTable(c echo.Context) error {
+	// 一度作ってみるのに使った後は間違って作りまくってしまわないようにコメントアウトでもしておくこと
+	db.CreateTable(&Ideas{})
+	return c.String(http.StatusOK, "Hello, World.\n")
 }
